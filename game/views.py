921399391d975c4db88dd561f4dd10aaada27737
blockchainGuru1018 <as20190101@hotmail.com>
2022-09-11 22:48:42 +0800

    add log

--- game/views.py
+++ game/views.py
@@ -17,7 +17,7 @@ class TriggerTableView(GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        print('-------------------------------', request)
+        print('-------------------------------', request.data)
         params = serializer.data.get('params')
         request_id = serializer.data.get('id')
 
