801b5200776d8d1826df32ca2d14fabbd71c656c
blockchainGuru1018 <as20190101@hotmail.com>
2022-11-24 11:53:44 +0800

    refactor action part

--- game/views.py
+++ game/views.py
@@ -28,18 +28,12 @@ class TriggerTableView(GenericAPIView):
         table_name = ''
 
         key = params['key']
-        print('--------fieldData--------', params['fieldData'])
-
         if 'personal_access_token' in params['fieldData']:
             personal_access_token = params['fieldData']['personal_access_token']
         if 'app_id' in params['fieldData']:
             app_id = params['fieldData']['app_id']
         if 'table_name' in params['fieldData']:
             table_name = params['fieldData']['table_name']
-
-        print('--------personal_access_token--------', personal_access_token)
-        print('--------app_id--------', app_id)
-        print('--------table_name--------', table_name)
 
         app_list = []
         table_list = []
@@ -220,21 +214,162 @@ class FirstRowView(GenericAPIView):
         params = serializer.data.get('params')
         request_id = serializer.data.get('id')
 
-        api_key = ''
+        personal_access_token = ''
         app_id = ''
         table_name = ''
 
         key = params['key']
-        if 'api_key' in params['fieldData']:
-            api_key = params['fieldData']['api_key']
+        if 'personal_access_token' in params['fieldData']:
+            personal_access_token = params['fieldData']['personal_access_token']
         if 'app_id' in params['fieldData']:
             app_id = params['fieldData']['app_id']
         if 'table_name' in params['fieldData']:
             table_name = params['fieldData']['table_name']
 
-        if api_key != '' and app_id != '' and table_name != '':
+        app_list = []
+        table_list = []
+        if personal_access_token != '' and app_id == '' and table_name == '':
+            header_my = {
+                'Authorization': 'Bearer ' + personal_access_token,
+                'Content-Type': 'application/json'
+            }
+            url = "https://api.airtable.com/v0/meta/bases"
+            res_app_list = requests.get(headers=header_my, url=url)
+
+            if res_app_list.status_code == 200:
+                for an_app in json.loads(res_app_list.content)['bases']:
+                    app_list.append({
+                        **serialize_base(an_app)
+                    })
+
+            return Response(
+                {
+                    "jsonrpc": "2.0",
+                    "id": request_id,
+                    "result": {
+                        "inputFields": [
+                            {
+                                "key": "personal_access_token",
+                                "label": "Airtable Personal access token",
+                                "type": "string",
+                                "placeholder": "patTPTGCSDECZm7i.3fa8581g1r52a52cb5a224sb8arccaf2ae82df346cd685ee766ff939dcb879be6",
+                                "helpText": "To get Personal access token, visit your account page: https://airtable.com/create/tokens.",
+                                "list": False,
+                                "required": True,
+                                "default": "",
+                                "computed": False
+                            },
+                            {
+                                "key": "app_id",
+                                "label": "Base",
+                                "helpText": "",
+                                "type": "string",
+                                "required": True,
+                                "placeholder": "Choose...",
+                                "choices": app_list
+                            }
+                        ]
+                    }
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        elif personal_access_token != '' and app_id != '' and table_name == '':
+            header_my = {
+                'Authorization': 'Bearer ' + personal_access_token,
+                'Content-Type': 'application/json'
+            }
+            url = "https://api.airtable.com/v0/meta/bases"
+            res_app_list = requests.get(headers=header_my, url=url)
+
+            if res_app_list.status_code == 200:
+                for an_app in json.loads(res_app_list.content)['bases']:
+                    app_list.append({
+                        **serialize_base(an_app)
+                    })
+
+            header = {
+                'Authorization': 'Bearer ' + personal_access_token,
+                'Content-Type': 'application/json'
+            }
+            url = "https://api.airtable.com/v0/meta/bases/" + app_id + "/tables"
+            res_table_list = requests.get(headers=header, url=url)
+
+            if res_table_list.status_code == 200:
+                for a_table in json.loads(res_table_list.content)['tables']:
+                    table_list.append({
+                        **serialize_table(a_table)
+                    })
+
+            return Response(
+                {
+                    "jsonrpc": "2.0",
+                    "id": request_id,
+                    "result": {
+                        "inputFields": [
+                            {
+                                "key": "personal_access_token",
+                                "label": "Airtable Personal access token",
+                                "type": "string",
+                                "placeholder": "patTPTGCSDECZm7i.3fa8581g1r52a52cb5a224sb8arccaf2ae82df346cd685ee766ff939dcb879be6",
+                                "helpText": "To get Personal access token, visit your account page: https://airtable.com/create/tokens.",
+                                "list": False,
+                                "required": True,
+                                "default": "",
+                                "computed": False
+                            },
+                            {
+                                "key": "app_id",
+                                "label": "Base",
+                                "helpText": "",
+                                "type": "string",
+                                "required": True,
+                                "placeholder": "Choose...",
+                                "choices": app_list
+                            },
+                            {
+                                "key": "table_name",
+                                "label": "Table",
+                                "helpText": "Table must have at least one record with data. If your table is empty, please add an example record.",
+                                "type": "string",
+                                "required": True,
+                                "placeholder": "Choose...",
+                                "choices": table_list
+                            }
+                        ]
+                    }
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        if personal_access_token != '' and app_id != '' and table_name != '':
+            header_my = {
+                'Authorization': 'Bearer ' + personal_access_token,
+                'Content-Type': 'application/json'
+            }
+            url = "https://api.airtable.com/v0/meta/bases"
+            res_app_list = requests.get(headers=header_my, url=url)
+
+            if res_app_list.status_code == 200:
+                for an_app in json.loads(res_app_list.content)['bases']:
+                    app_list.append({
+                        **serialize_base(an_app)
+                    })
+
+            header = {
+                'Authorization': 'Bearer ' + personal_access_token,
+                'Content-Type': 'application/json'
+            }
+            url = "https://api.airtable.com/v0/meta/bases/" + app_id + "/tables"
+            res_table_list = requests.get(headers=header, url=url)
+
+            if res_table_list.status_code == 200:
+                for a_table in json.loads(res_table_list.content)['tables']:
+                    table_list.append({
+                        **serialize_table(a_table)
+                    })
             try:
-                table = Table(api_key, app_id, table_name)
+                table = Table(personal_access_token, app_id, table_name)
                 first_row = []
                 if table.first():
                     for key in table.first()['fields']:
@@ -251,31 +386,33 @@ class FirstRowView(GenericAPIView):
                             "result": {
                                 "inputFields": [
                                     {
-                                        "key": "api_key",
-                                        "label": "API key",
+                                        "key": "personal_access_token",
+                                        "label": "Airtable Personal access token",
                                         "type": "string",
-                                        "placeholder": "keyLOfGeYHXLqpeo0",
-                                        "helpText": "To get API key, visit your account page: https://airtable.com/account.",
+                                        "placeholder": "patTPTGCSDECZm7i.3fa8581g1r52a52cb5a224sb8arccaf2ae82df346cd685ee766ff939dcb879be6",
+                                        "helpText": "To get Personal access token, visit your account page: https://airtable.com/create/tokens.",
                                         "list": False,
-                                        "required": True
+                                        "required": True,
+                                        "default": "",
+                                        "computed": False
                                     },
                                     {
                                         "key": "app_id",
-                                        "label": "Base Id",
+                                        "label": "Base",
+                                        "helpText": "",
                                         "type": "string",
-                                        "placeholder": "appGgI109POdPOOKK",
-                                        "helpText": "You can get base ID from the base URL, or from API documentation at https://airtable.com/api.",
-                                        "list": False,
-                                        "required": True
+                                        "required": True,
+                                        "placeholder": "Choose...",
+                                        "choices": app_list
                                     },
                                     {
                                         "key": "table_name",
-                                        "label": "Table Name",
-                                        "type": "string",
-                                        "placeholder": "Table Name",
+                                        "label": "Table",
                                         "helpText": "Table must have at least one record with data. If your table is empty, please add an example record.",
-                                        "list": False,
-                                        "required": True
+                                        "type": "string",
+                                        "required": True,
+                                        "placeholder": "Choose...",
+                                        "choices": table_list
                                     },
                                 ] + first_row
                             }
@@ -290,31 +427,33 @@ class FirstRowView(GenericAPIView):
                             "result": {
                                 "inputFields": [
                                     {
-                                        "key": "api_key",
-                                        "label": "API key",
+                                        "key": "personal_access_token",
+                                        "label": "Airtable Personal access token",
                                         "type": "string",
-                                        "placeholder": "keyLOfGeYHXLqpeo0",
-                                        "helpText": "To get API key, visit your account page: https://airtable.com/account.",
+                                        "placeholder": "patTPTGCSDECZm7i.3fa8581g1r52a52cb5a224sb8arccaf2ae82df346cd685ee766ff939dcb879be6",
+                                        "helpText": "To get Personal access token, visit your account page: https://airtable.com/create/tokens.",
                                         "list": False,
-                                        "required": True
+                                        "required": True,
+                                        "default": "",
+                                        "computed": False
                                     },
                                     {
                                         "key": "app_id",
-                                        "label": "Base Id",
+                                        "label": "Base",
+                                        "helpText": "",
                                         "type": "string",
-                                        "placeholder": "appGgI109POdPOOKK",
-                                        "helpText": "You can get base ID from the base URL, or from API documentation at https://airtable.com/api.",
-                                        "list": False,
-                                        "required": True
+                                        "required": True,
+                                        "placeholder": "Choose...",
+                                        "choices": app_list
                                     },
                                     {
                                         "key": "table_name",
-                                        "label": "Table Name",
-                                        "type": "string",
-                                        "placeholder": "Table Name",
+                                        "label": "Table",
                                         "helpText": "Table must have at least one record with data. If your table is empty, please add an example record.",
-                                        "list": False,
-                                        "required": True
+                                        "type": "string",
+                                        "required": True,
+                                        "placeholder": "Choose...",
+                                        "choices": table_list
                                     },
                                     {
                                         "key": "warning",
