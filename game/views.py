c1644431bd73e3db37cf7a583f6037b0fe1b44ff
blockchainGuru1018 <as20190101@hotmail.com>
2022-11-24 10:08:16 +0800

    add logs

--- game/views.py
+++ game/views.py
@@ -28,6 +28,7 @@ class TriggerTableView(GenericAPIView):
         table_name = ''
 
         key = params['key']
+        print('--------fieldData--------', params['fieldData'])
         if 'personal_access_token' in params['fieldData']:
             personal_access_token = params['fieldData']['personal_access_token']
         if 'app_id' in params['fieldData']:
